84e8b0722b0d00e662b2e45a6d94cae5519f7b2f
zachj95 <40276139+zjones95@users.noreply.github.com>
2026-01-24 14:30:20 -0800

    move images, fix references to images

--- src/constants.ts
+++ src/constants.ts
@@ -2,52 +2,52 @@ import type { ImageData } from "./types";
 
 export const IMAGES: ImageData[] = [
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 1",
     variant: "square",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 2",
     variant: "square",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 3",
     variant: "square",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 2",
     variant: "wide",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 2",
     variant: "wide",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 1",
     variant: "square",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 2",
     variant: "wide",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 3",
     variant: "square",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 2",
     variant: "square",
   },
   {
-    src: "/public/when-do-dogs-stop-growing.webp",
+    src: "src/assets/when-do-dogs-stop-growing.webp",
     alt: "Image 2",
     variant: "square",
   },
