847b6de479b11798912df4505e7f32f9a123e87b
zachj95 <40276139+zjones95@users.noreply.github.com>
2026-01-25 02:30:30 -0800

    hook up contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,71 +1,90 @@
-import { useState } from "react";
-import { Button, Stack, TextField, Typography } from "@mui/material";
-
-type FormData = {
-  name: string;
-  email: string;
-  description: string;
-};
+import { useRef, useState } from "react";
+import {
+  Alert,
+  Button,
+  Snackbar,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  const [formData, setFormData] = useState<FormData>({
-    name: "",
-    email: "",
-    description: "",
-  });
-
-  const handleChange = (fieldName: keyof FormData, value: string) => {
-    setFormData({
-      ...formData,
-      [fieldName]: value,
-    });
-  };
+  const [showSuccess, setShowSuccess] = useState(false);
+  const formRef = useRef<HTMLFormElement | null>(null);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!formRef.current) {
+      return;
+    }
+
+    emailjs
+      .sendForm("service_jcjqat4", "template_bbtdllj", formRef.current, {
+        publicKey: "user_9q7DlrkhVKyOQX0hE4im9",
+      })
+      .then(() => {
+        setShowSuccess(true);
+        formRef.current?.reset();
+      });
   };
 
-  const isFormFilled = Object.values(formData).every((v) => v.length > 0);
+  const handleHideSuccess = () => {
+    setShowSuccess(false);
+  };
 
   return (
-    <Stack
-      component="form"
-      sx={{
-        maxWidth: "600px",
-        width: "100%",
-        margin: "auto",
-      }}
-      spacing={4}
-      onSubmit={handleSubmit}
-    >
-      <Typography variant="h2" fontSize={32}>
-        Contact
-      </Typography>
-      <TextField
-        id="contact-name"
-        label="Name"
-        variant="filled"
-        onChange={(e) => handleChange("name", e.target.value)}
-      />
-      <TextField
-        id="contact-email"
-        label="Email"
-        variant="filled"
-        type="email"
-        onChange={(e) => handleChange("email", e.target.value)}
-      />
-      <TextField
-        id="contact-description"
-        label="Description"
-        variant="filled"
-        rows={4}
-        multiline
-        onChange={(e) => handleChange("description", e.target.value)}
-      />
-      <Button type="submit" variant="contained" disabled={!isFormFilled}>
-        Submit
-      </Button>
-    </Stack>
+    <>
+      <Stack
+        component="form"
+        sx={{
+          maxWidth: "600px",
+          width: "100%",
+          margin: "auto",
+        }}
+        spacing={4}
+        onSubmit={handleSubmit}
+        ref={formRef}
+      >
+        <Typography variant="h2" fontSize={32}>
+          Contact
+        </Typography>
+        <TextField id="contact-name" label="Name" variant="filled" required />
+        <TextField
+          id="contact-email"
+          label="Email"
+          variant="filled"
+          type="email"
+          required
+        />
+        <TextField
+          id="contact-description"
+          label="Description"
+          variant="filled"
+          rows={4}
+          multiline
+          required
+        />
+        <Button type="submit" variant="contained">
+          Submit
+        </Button>
+      </Stack>
+      <Snackbar
+        open={showSuccess}
+        autoHideDuration={4000}
+        onClose={handleHideSuccess}
+      >
+        <Alert
+          onClose={handleHideSuccess}
+          severity="success"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          Message sent successfully!
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
